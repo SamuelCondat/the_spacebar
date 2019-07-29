370b50429d693d36b5e517b41c46352a4efa8c58
Samuel <samuelcondat@gmail.com>
2019-07-29 14:19:23 +0200

    making so much good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+}
+
+
